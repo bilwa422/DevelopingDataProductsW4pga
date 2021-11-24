51d116b5ec0d9413dbc478b58541d525e32ac423
bilwa422 <bilwasharma98@gmail.com>
2021-11-24 13:14:16 +0530

    Hello

---
+++ server.R
@@ -0,0 +1,31 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+library(shiny)
+library(datasets)
+library(dplyr)
+shinyServer(function(input, output) {
+  
+  # Show the cars that correspond to the filters
+  
+  output$table <- renderDataTable({
+    
+    hp_seq <- seq(from = input$hp[1], to = input$hp[2], by = 1)
+    avg_sq <- seq(from = input$avg[1], to = input$avg[2], by = .1)
+    
+    data <- transmute(mtcars, Car = rownames(mtcars), Milage = mpg, 
+                      Cylinders = cyl, Horsepower = hp, 
+                      Transmission = am, Gears=gear, Quater_Mile_Time= qsec)
+    data <- filter(data, Milage %in% avg_sq, Cylinders %in% input$cyl,Horsepower %in% hp_seq, Transmission %in% input$am, Gears %in% input$gear)
+    data <- mutate(data, Transmission = ifelse(Transmission==0, "Automatic", "Manual"))
+    
+    data
+    
+  }, options = list(lengthMenu = c(5, 15, 30), pageLength = 30))
+  
+})
